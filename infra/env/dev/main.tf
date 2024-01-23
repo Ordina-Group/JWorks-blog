9aa19ee2874ef7145591c6cc562e88c7d976b566
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-01-23 11:10:22 +0100

    add access control allow origins

--- infra/env/dev/main.tf
+++ infra/env/dev/main.tf
@@ -1,9 +1,11 @@
 module "s3_static_hosting" {
-  source           = "github.com/Ordina-Group/jworks-aws-infra.git//landing-zones/static-website?ref=4acf7743ed3484ff0051f5cab32199a647ed8a53"
-  app_name         = "blog"
-  bucket_name      = "blog.ordina-jworks.io"
-  region           = var.aws_region
-  root_domain_name = "tst.ordina-jworks.io"
+  source                       = "github.com/Ordina-Group/jworks-aws-infra.git//landing-zones/static-website?ref=4acf7743ed3484ff0051f5cab32199a647ed8a53"
+  app_name                     = "blog"
+  bucket_name                  = "blog.ordina-jworks.io"
+  region                       = var.aws_region
+  root_domain_name             = "tst.ordina-jworks.io"
+  access_control_allow_origins = ["https://blog.ordina-jworks.io"]
+
 }
 
 module "s3_uploader" {
