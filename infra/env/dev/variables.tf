566001b24af5ea9309e7fbd3978b321b523395d5
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-01-24 08:19:26 +0100

    update csp style

--- infra/env/dev/variables.tf
+++ infra/env/dev/variables.tf
@@ -5,6 +5,6 @@ variable "aws_region" {
 
 variable "content_security_policy" {
   type        = string
-  default     = "frame-ancestors 'none'; default-src 'none'; img-src 'self'; script-src 'self'; script-src-elem 'self' 'sha256-/rbuLmjOhdrXDXnFnTI3s+BYoAyRHYELKon3OljZ3I8=' 'sha256-QKOHfaooBSX/Bn5NbamY975gpno/BvAGbjsuLDRYT5M=' 'sha256-6keeQcu9Kh8gXXIlOpmNHs7f2UzbAyMVaH+sBk0iDwo=' use.typekit.net; style-src 'self' 'unsafe-hashes'; font-src 'self'; object-src 'none';"
+  default     = "frame-ancestors 'none'; default-src 'none'; img-src 'self'; script-src 'self'; script-src-elem 'self' 'sha256-/rbuLmjOhdrXDXnFnTI3s+BYoAyRHYELKon3OljZ3I8=' 'sha256-QKOHfaooBSX/Bn5NbamY975gpno/BvAGbjsuLDRYT5M=' 'sha256-6keeQcu9Kh8gXXIlOpmNHs7f2UzbAyMVaH+sBk0iDwo=' use.typekit.net; style-src 'self' 'unsafe-hashes' 'unsafe-inline'; font-src 'self'; object-src 'none';"
   description = "The policy directives and their values that CloudFront includes as values for the Content-Security-Policy HTTP response header."
 }
