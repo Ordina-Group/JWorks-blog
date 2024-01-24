94a21ebcd3d03c22e48ff82c1fedd1f6aa030a4d
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-01-24 10:24:15 +0100

    update csp

--- infra/env/dev/variables.tf
+++ infra/env/dev/variables.tf
@@ -5,6 +5,6 @@ variable "aws_region" {
 
 variable "content_security_policy" {
   type        = string
-  default     = "frame-ancestors 'none'; default-src 'none'; img-src 'self' data: jworks-techblog-analytics.cfapps.io p.typekit.net; script-src 'self'; script-src-elem 'self' 'unsafe-inline' use.typekit.net www.google-analytics.com; connect-src www.google-analytics.com; style-src 'self' 'unsafe-inline'; font-src 'self' use.typekit.net; object-src 'none'; frame-src www.youtube.com"
+  default     = "frame-ancestors 'none'; default-src 'none'; img-src 'self' data: jworks-techblog-analytics.cfapps.io p.typekit.net; script-src 'self'; script-src-elem 'self' 'unsafe-inline' use.typekit.net www.google-analytics.com www.googletagmanager.com; connect-src www.google-analytics.com doubleclick.net; style-src 'self' 'unsafe-inline'; font-src 'self' use.typekit.net; object-src 'none'; frame-src www.youtube.com"
   description = "The policy directives and their values that CloudFront includes as values for the Content-Security-Policy HTTP response header."
 }
