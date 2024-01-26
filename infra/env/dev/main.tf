f2a5fb09d859b1914074f2370d01ba0d8a2d6ce3
Ferre Vangenechten <ferre.vangenechten@ordina.be>
2024-01-26 16:24:59 +0100

    chore(ci): dynamic region and rule group name

--- infra/env/dev/main.tf
+++ infra/env/dev/main.tf
@@ -15,11 +15,11 @@ module "s3_static_hosting" {
 }
 
 module "waf" {
-  source            = "github.com/Ordina-Group/jworks-aws-infra.git//waf-module?ref=248d1fffbd7acb467922034e2ebe150d60e71c1e"
-  project_name      = "waf-jworks-tech-blog"
-  cloudfront        = true
-  blocked_countries = ["RU"]
-  region = "us-east-1"
+  source              = "github.com/Ordina-Group/jworks-aws-infra.git//waf-module?ref=248d1fffbd7acb467922034e2ebe150d60e71c1e"
+  project_name        = "waf-jworks-tech-blog"
+  cloudfront          = true
+  blocked_countries   = ["RU"]
+  region              = "us-east-1"
   waf_rule_group_name = "waf-jworks-tech-blog-rule-group"
 }
 
