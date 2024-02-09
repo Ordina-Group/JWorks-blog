10fe93dbea076ba8c183fbf944ccf4c0359b0012
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-02-09 11:30:41 +0100

    deploy on AWS (#6)
    ### Thank you for your contribution! Please go through this small
    checklist to prepare your pull request to be reviewed as soon as
    possible :)
    * [ ] All authors were added to `_data/authors.yml` with a picture and
    an up-to-date bio
    * [ ] The blog post image is of ratio 500x293
    * Feel free to leave a comment if you don't know how to resize your
    image and someone will gladly help you out
    * [ ] The [**one sentence per line**
    method](https://raw.githubusercontent.com/brandon-rhodes/blog/master/texts/brandon/2012/one-sentence-per-line.rst)
    has been adopted
    * [ ] A spell check has been done
    * [ ] All links open in a new tab
    * Example: `[Amazing tech
    blog](https://ordina-jworks.github.io){:target="_blank" rel="noopener
    noreferrer"}`
    * [ ] The post's content properly resizes according to the window's size
    * Tip: Just shorten your window's horizontal length to easily test this
      * To have images properly resize use `class="image fit"`
    * To have YouTube videos properly resize, wrap the iframe into a `<div
    class="responsive-video">`
    * [ ] If a table of content is: check whether all links are working
    correctly
    
    ### Any specific remarks for the reviewers or anything you need help
    with?
    \<enter text here>
    
    ### Checklist for the reviewers before merging
    * [ ] Publishing date of the post has been correctly set
    * This can be done by renaming the filename or by setting the date in
    the header section of the post
    
    ---------
    
    Co-authored-by: Ferre Vangenechten <ferre.vangenechten@ordina.be>
    Co-authored-by: Yolan Vloeberghs <yolanvloeberghs@gmail.com>

---
+++ infra/env/dev/config.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "jworks-tech-blog-dev-terraform-state"
+    key    = "blog.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      "app" = "jworks-tech-blog"
+    }
+  }
+}
---
+++ infra/env/prd/main.tf
@@ -0,0 +1,24 @@
+module "s3_static_hosting" {
+  source                       = "github.com/Ordina-Group/jworks-terraform-modules.git//static-website?ref=v1.0.0"
+  app_name                     = "blog"
+  bucket_name                  = "blog.ordina-jworks.io"
+  region                       = var.aws_region
+  root_domain_name             = "dev.ordina-jworks.io"
+  access_control_allow_headers = ["blog"]
+  access_control_allow_methods = ["GET"]
+  access_control_allow_origins = ["https://blog.dev.ordina-jworks.io"]
+  content_security_policy      = var.content_security_policy
+  cross_origin_embedder_policy = "unsafe-none"
+  cross_origin_opener_policy   = "unsafe-none"
+  cross_origin_resource_policy = "unsafe-none"
+  web_acl_id                   = module.waf.web_acl_arn
+}
+
+module "waf" {
+  source              = "github.com/Ordina-Group/jworks-terraform-modules.git//waf-module?ref=v1.0.0"
+  project_name        = "waf-jworks-tech-blog"
+  cloudfront          = true
+  blocked_countries   = ["RU"]
+  region              = "us-east-1"
+  waf_rule_group_name = "waf-jworks-tech-blog-rule-group"
+}
